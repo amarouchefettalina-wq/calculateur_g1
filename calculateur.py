d158b84871c7c4725a5924b35ea4bbf678a43afb
fettalina-wq <amarouchefettalina@gmail.com>
2026-05-30 21:29:46 +0200

    modif

--- calculateur.py
+++ calculateur.py
@@ -1,5 +1,5 @@
-def calculer_tva(prix_ht: float)-> float:
-     return prix_ht * 1.20
+def calculer_tva(prix_ht, taux_tva):
+    return prix_ht * 120
 
 prix_ttc = calculer_tva(100)
 print(f"total TTC: {prix_ttc}")
